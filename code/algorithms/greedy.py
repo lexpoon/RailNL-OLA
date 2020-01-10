13b1b0d679b0f2bb0faab5572c9a84686b01fe60
Annejet <annejetrobijn@gmail.com>
2020-01-10 14:29:13 +0100

    update greedy.py

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -7,3 +7,39 @@ from classes.solution import Solution
 def greedy(routes, time):
     """ Create solution consisting of routes based on greedy algorithm. """
     
+    max_routes = routes
+    max_time = time
+
+    data = RailNL().data
+    solution = {}
+    solution["routes"] = []
+
+    # Number of connections in data
+    all_connections = calc_connections()
+    used_connections = set()
+
+    # While max routes not reached AND not all connections are used
+    while len(solution["routes"]) < max_routes and len(used_connections) < all_connections:
+        solution["routes"].append(greedy_route(solution))
+        used_connections.update(calc_used_connections(solution["routes"]))
+
+    greedy_solution = Solution(solution["routes"])
+    solution["time"] = greedy_solution.time
+    solution["quality"] = greedy_solution.score
+
+    return greedy_solution
+
+def greedy_solution(solution):
+    """ Find route based on greedy algorithm. """
+
+    data = RailNL().data
+    route_list = []
+    total_time = 0
+
+    route_list.append(data[random.choice(list(data.keys()))])
+
+
+
+    return route_list
+
+
