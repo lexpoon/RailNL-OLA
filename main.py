b09b9df0f420d4e301f1a33bed0eec8e93dd04e1
Annejet <annejetrobijn@gmail.com>
2020-01-10 14:29:42 +0100

    update main.py with visualisation

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ from solution import Solution
 from import_data import RailNL
 from calculations import calc_stations, calc_connections
 from randomize import solution
+from visualize import visualisation
 
 def main():
     data = RailNL()
     solution_random = solution(7, 120)
 
+    # TEST
+    test_routes_visualisation = {'1': ['Amsterdam Zuid', 'Amsterdam Amstel', 'Schiphol Airport'], '2': ['Dordrecht', 'Rotterdam Centraal', 'Schiedam Centrum', 'Delft', 'Den Haag Centraal', 'Leiden Centraal']}
+    visualize = visualisation(test_routes_visualisation)
+    # return visualize
+    
     return solution_random
 
 if __name__ == "__main__":
