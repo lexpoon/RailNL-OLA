c7097de730b47702d659c59f7a19e5be221908be
Annejet <annejetrobijn@gmail.com>
2020-01-23 11:12:29 +0100

    update hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -6,6 +6,7 @@ from classes.solution import Solution
 from greedy import greedy
 
 import copy
+import random
 
 def hillclimber(greedy_output, time, map, min_score):
     """"Create hillclimber solution based on greedy output"""
@@ -28,7 +29,7 @@ def hillclimber(greedy_output, time, map, min_score):
     new_score = Solution(greedy_output.routes, map).score
     improvement = new_score - old_score
 
-    print (f"Improvement: {improvement} with new score: {new_score}")
+    print (f"First improvement: {improvement} with new score: {new_score}")
 
     # If all connections are used, return solution
     unused_connections = calc_connections(map) - calc_used_connections(Solution(greedy_output.routes, map).routes)
@@ -38,23 +39,32 @@ def hillclimber(greedy_output, time, map, min_score):
     data = RailNL(map).data
     solution = Solution(greedy_output.routes, map)
 
+    improving = True
+
     while True:
+
+        solution = Solution(greedy_output.routes, map)
 
         # Update connections that are left
         connections_left = update_connections(solution.routes, data, map)['amount_connections']
 
         # Find station in route that has an unused connection
-        for route in solution.routes:
-            for station in route.route:
+        for traject in solution.routes:
+            for station in traject.route:
                 if str(station) in connections_left.keys():
                     if connections_left[str(station)] >= 1:
-                        index = route.route.index(station)
-                        first_part = list(route.route[:index+1])
-                        last_part = list(route.route[index:])
+                        index = traject.route.index(station)
+                        first_part = list(traject.route[:index+1])
+                        last_part = list(traject.route[index:])
                         
                         # Find neighbour station that has no connection
                         unused_connections = calc_connections(map) - calc_used_connections(solution.routes)
-                        
+
+                        if unused_connections == set():
+                            return False
+                            print('no unused connections anymore')
+                            # improving = False
+
                         for connection in unused_connections:
                             if str(connection[0])==str(station):
                                 new_station = data[str(connection[1])]
@@ -64,7 +74,7 @@ def hillclimber(greedy_output, time, map, min_score):
                                                     
                         # Create temporary route with unconnected station added and calculate new score
                         temp_route = first_part + [new_station] + last_part
-                        route_index = solution.routes.index(route) 
+                        route_index = solution.routes.index(traject)
                         copy_routes = copy.deepcopy(solution)
                         copy_routes.routes[route_index].route = Route([temp_route], map).route
                         temp_score = Solution(copy_routes.routes, map).score
@@ -74,13 +84,88 @@ def hillclimber(greedy_output, time, map, min_score):
                         if temp_score > score_original:
                             greedy_output.routes[route_index] = copy_routes.routes[route_index]
                             solution = Solution(greedy_output.routes, map)
-
-                            print('Old score:', score_original)
                             print('Added station:', new_station)
-                            print('Improved new score:', temp_score)
-                                                  
+                            print('Improved to:', temp_score)
+                            # continue
+                        
                         else:
-    
-                            solution = 'Final score: ' + str(Solution(greedy_output.routes, map).score)
-                            print(solution)
-                            return Solution(greedy_output.routes, map)
+                            return False
+                            print('not improving, end while loop')
+                            # improving = False
+                            test(greedy_output.routes, map)
+                            # break
+                                              
+    # s = ('Final score: ', str(Solution(greedy_output.routes, map).score))
+    # return s
+
+    # print(Solution(greedy_output.routes, map))
+
+# def test(solution, map):
+#     print(Solution(greedy_output.routes, map))
+
+    # for i in range(20):
+
+    #     # Create list of stations that are end/begin stations and have more than 1 possible connection
+    #     stations = []
+    #     for traject in greedy_output.routes:
+    #         for station in traject.route:
+    #             if (traject.route.index(station)==0 or traject.route.index(station)==len(traject.route)) and len(station.connections) > 1:
+    #                 stations.append(station)
+
+    #     # Get random station from stations list
+    #     station = random.choice(stations)
+
+    #     # Find route to which random station belongs
+    #     route_object = [traject for traject in greedy_output.routes if (station in traject.route) and (traject.route.index(station)==0 or traject.route.index(station)==len(traject.route)) and len(station.connections)>1][0]
+    #     route_list = route_object.route
+    #     route_index = greedy_output.routes.index(route_object)
+
+    #     # Randomly choose station to connect with
+    #     connection = random.choice(station.connections)
+        
+        # Create new route with station added at the start or end of the original route
+        # if route_list.index(station) == len(route_list):
+        #     # new_connection = (station, connection[0])
+        #     new_station = str(connection[0])
+        #     print('---', new_station)
+        #     print('///', route_list)
+        #     new_route = route_list.append(new_station)
+        #     print('new route:', new_route)
+        #     # if Route(new_route).time < time:
+        #     print('Swapped: ', new_station)
+        #     greedy_output.routes[route_index].route = Route(new_route, map).route
+
+        # if route_list.index(station) == 0:
+        #     # new_connection = (connection[0], station)
+        #     new_station = str(connection[0])
+        #     print('---', new_station)
+        #     print('///', route_list)
+        #     # print('list:',route_list)
+        #     # print('connection:',connection[0])
+        #     new_route = [new_station] + route_list
+        #     print('new route:', new_route)
+        #     # if ...:
+        #     print('Swapped: ', new_station)
+        #     greedy_output.routes[route_index].route = Route(new_route, map).route        
+
+        # check op tijd
+        # als je in het midden komt, 
+
+
+        # Delete first or last station from route
+        # for route in greedy_output.routes:
+        #     for connection in route.route:
+        #         if connection = new_connection:
+        #             if new_connection[0] == station:
+        #                 routes[route].route = route[:-1]
+        #             routes[route].route = route[1:]
+
+        # solution = solution
+
+        # return Solution(greedy_output.routes, map)
+
+
+
+
+
+
