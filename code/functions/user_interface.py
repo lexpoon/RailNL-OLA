d672ae80bd790ca0799617702e04ab2edd52715f
Annejet <annejetrobijn@gmail.com>
2020-01-27 12:41:30 +0100

    improve style user interface

--- code/functions/user_interface.py
+++ code/functions/user_interface.py
@@ -1,8 +1,72 @@
+from functions.run_algorithm import run_algorithm, start_algorithm, improve_algorithm
+from functions.calculations import all_connections, connections_station, update_connections
+
+def user_interface():
+    """Start user interface"""
+
+    welcome()
+    map_info = get_map_info()
+    algorithm_info = get_create_algorithm()
+
+    map = map_info[0]
+    max_routes = map_info[1]
+    max_time = map_info[2]
+    solution = None
+    algorithm = algorithm_info[0]
+    iterations = ''
+    key = algorithm_info[1]
+    depth = algorithm_info[2]
+    ratio = algorithm_info[3]
+    remove_routes = None
+    formula = None
+    definition = "create"
+
+    while isinstance(iterations, int) == False:
+        iterations = get_int("Hoevaak wil je een nieuwe oplossing genereren?\n")
+
+    solution = run_algorithm(
+            map, max_routes, max_time, solution, algorithm, iterations, key, depth, 
+            ratio, remove_routes, formula, definition)
+
+    best_solution = {"solution": '', "score": 0}
+    best_solution = check_score(best_solution, solution)
+
+    while True:
+        next = next_step()
+        solution = solution
+        algorithm = next[0]
+        iterations = ''
+        key = next[1]
+        depth = next[2]
+        ratio = next[3]
+        remove_routes = None
+        formula = next[4]
+        definition = next[5]
+
+        if next == "stop":
+            break
+
+        elif next[5] == "improve" and algorithm!="short_route_swap":
+            while isinstance(remove_routes, int) == False and remove_routes==None:
+                remove_routes = get_int("Hoeveel nieuwe routes per keer wil je genereren?\n")
+                if remove_routes >= max_routes:
+                    remove_routes = None
+
+        while isinstance(iterations, int) != True and algorithm!="short_route_swap":
+            iterations = get_int("Hoevaak wil je een oplossing genereren?\n")
+
+        solution = run_algorithm(
+            map, max_routes, max_time, solution, algorithm, iterations, key, depth, 
+            ratio, remove_routes, formula, definition)
+
+        best_solution = check_score(best_solution, solution)
+
+
 def welcome():
     """Welcome user"""
 
     print("Welkom bij RailNL!")
-    print("We gaan proberen een zo goed mogelijke dienstregeling vinden.")
+    print("We gaan proberen een zo goed mogelijke intercity dienstregeling vinden.")
 
 
 def get_map_info():
@@ -11,7 +75,9 @@ def get_map_info():
     map = ''
 
     while map != "holland" and map != "h" and map != "nationaal" and map != "n":
-        map = input("Voor welke map wil je een oplossing generen? Holland (h/H) of Nationaal (n/N)? \n").lower()
+        map = input(
+            "Voor welke map wil je een oplossing generen? Holland (h/H) of Nationaal (n/N)? \n"
+        ).lower()
 
     if map == "holland" or map == "h":
         map = "Holland"
@@ -32,7 +98,9 @@ def get_create_algorithm():
     algorithm = ''
 
     while algorithm != "random" and algorithm != "r" and algorithm != "greedy" and algorithm != "g" and algorithm != "depth_first" and algorithm != "d" and algorithm != "breadth_first" and algorithm != "b":
-        algorithm = input("Met welk algoritme wil je een oplossing generen? Random (R/r), Greedy (G/g), Depth First (D/p) of Bread First (B/b)?\n").lower()
+        algorithm = input(
+            "Met welk algoritme wil je een oplossing generen? Random (R/r), Greedy (G/g), Depth First (D/p) of Bread First (B/b)?\n"
+        ).lower()
 
         if algorithm == "random" or algorithm == "r":
             algorithm = "random"
@@ -44,7 +112,9 @@ def get_create_algorithm():
             depth = ratio = None
 
             while key != "connecties" and key != "c" and key != "tijd" and key != "t" and key != "score" and key != "s":
-                key = input("Op basis waarvan wil je het Greedy algoritme runnen? Connecties (C/c), Tijd (T/t) of Score (S/s)?\n").lower()
+                key = input(
+                    "Op basis waarvan wil je het Greedy algoritme runnen? Connecties (C/c), Tijd (T/t) of Score (S/s)?\n"
+                ).lower()
 
             if key == "connecties" or key == "c":
                 key = "connections"
@@ -64,7 +134,9 @@ def get_create_algorithm():
                 depth = get_int("Na hoeveel stations wil je beginnen met prunen?\n")
 
             while isinstance(ratio, float) != True:
-                ratio = get_float("Bij welke ratio (score/lengte route) achterstand op de beste score wil je stoppen met zoeken?\n")
+                ratio = get_float(
+                    "Bij welke ratio (score/lengte route) achterstand op de beste score wil je stoppen met zoeken?\n"
+                )
 
         elif algorithm == "breadth first" or algorithm == "b":
             algorithm = "breadth_first"
@@ -75,7 +147,9 @@ def get_create_algorithm():
                 depth = get_int("Na hoeveel stations wil je beginnen met prunen?\n")
 
             while isinstance(ratio, float) != True:
-                ratio = get_float("Bij welke ratio (score/lengte route) achterstand op de beste score wil je stoppen met zoeken?\n")
+                ratio = get_float(
+                    "Bij welke ratio (score/lengte route) achterstand op de beste score wil je stoppen met zoeken?\n"
+                )
 
     return [algorithm, key, depth, ratio]
 
@@ -87,7 +161,9 @@ def get_improve_algorithm():
     formula = None
 
     while algorithm != "hillclimber" and algorithm != "h" and algorithm != "short route swap" and algorithm != "s" and algorithm != "simulated annealing" and algorithm != "a":
-        algorithm = input("Met welk algoritme wil je een oplossing generen? Short Route Swap (S/s), Hillclimber (H/h), Simulated Annealing (A/a)?\n").lower()
+        algorithm = input(
+            "Met welk algoritme wil je een oplossing generen? Short Route Swap (S/s), Hillclimber (H/h), Simulated Annealing (A/a)?\n"
+        ).lower()
 
     if algorithm == "hillclimber" or algorithm == "h" or algorithm == "simulated annealing" or algorithm == "a":
         route_info = get_create_algorithm()
@@ -99,7 +175,9 @@ def get_improve_algorithm():
             algorithm = "simulated_annealing"
 
             while formula != "linear" and formula != "l" and formula != "exponential" and formula != "e":
-                formula = input("Met welk temperatuur wil je de temperatuur generen? Linear (L/l) of Exponentieel (E/e)\n").lower()
+                formula = input(
+                    "Met welk temperatuur wil je de temperatuur generen? Linear (L/l) of Exponentieel (E/e)\n"
+                ).lower()
 
                 if formula == "linear" or formula == "l":
                     formula = "linear"
@@ -130,7 +208,9 @@ def next_step():
     next_step = ''
 
     while next_step != "v" and next_step != "n" and next_step != "q":
-        next_step = input("Opties: Gebruik verbeter algoritme (V/v), Nieuwe oplossing genereren (N/n), Stoppen (Q/q)\n").lower()
+        next_step = input(
+            "Gebruik verbeter algoritme (V/v), Nieuwe oplossing genereren (N/n), of Stoppen (Q/q)\n"
+            ).lower()
 
     if next_step == 'v':
         info = get_improve_algorithm()
@@ -142,7 +222,7 @@ def next_step():
         definition = "create"
 
     else:
-        return "Stoppen"
+        return "stop"
 
     return [info[0], info[1], info[2], info[3], info[4], definition]
 
